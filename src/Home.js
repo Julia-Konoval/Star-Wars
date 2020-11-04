d0bd15167b4df2c4a6feb89bc9e4ffb6ea86220f
Julia <yulyakonoval1998@gmail.com>
2020-11-04 22:07:51 +0200

    link to planetInfo was added

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,46 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Planet from "./Planet";
+import "./Home.css";
 
 function Home() {
   const [planets, setPlanets] = useState([]);
-  
+  const planetList = 0;
+  useEffect(() => {
+    async function fetchData() {
+      let i = 1;
+      let results = [];
+      while (true) {
+        try {
+          const response = await axios.get(
+            `https://swapi.dev/api/planets/?page=${i}`
+          );
+          console.log(response.data.results);
+          results = results.concat(response.data.results);
+          i++;
+        } catch (error) {
+          console.log(error);
+          break;
+        }
+      }
+      console.log(results);
+      setPlanets(results);
+      console.log(planets);
+    }
+
+    fetchData();
   }, []);
   return (
-    
+    <div className="home_container">
+      <h1>Star Wars Planets</h1>
+      <div className="planet_list_container">
+        {planets &&
+          planets.map((planet, index) => (
+            <Planet planet={planet} key={index} />
+          ))}
+      </div>
+    </div>
   );
-
+}
 
 export default Home;
--- src/Planet.js
+++ src/Planet.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Planet.css";
+import { Link } from "react-router-dom";
 
 function Planet({ planet }) {
   let formatPopulation = String(planet.population).replace(
@@ -7,11 +8,18 @@ function Planet({ planet }) {
     "$1,"
   );
   return (
-    <div className="planet_container">
-      <h2>{planet.name}</h2>
-      <p>{planet.climate}</p>
-      <p>{formatPopulation}</p>
-    </div>
+    <Link
+      to={{
+        pathname: `/planet/${planet.name.replace(/ /g, "_")},`,
+        state: { planet },
+      }}
+    >
+      <div className="planet_container">
+        <h2>{planet.name}</h2>
+        <p>{planet.climate}</p>
+        <p>{formatPopulation}</p>
+      </div>
+    </Link>
   );
 }
 
