4144896bb166267cacd52d360a34bc8f29588d94
Julia <yulyakonoval1998@gmail.com>
2020-11-04 21:22:53 +0200

    population number formatted

--- src/Planet.js
+++ src/Planet.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./Planet.css";
 
 function Planet({ planet }) {
+  let formatPopulation = String(planet.population).replace(
+    /(.)(?=(\d{3})+$)/g,
+    "$1,"
+  );
   return (
     <div className="planet_container">
       <h2>{planet.name}</h2>
       <p>{planet.climate}</p>
-      <p>{planet.population}</p>
+      <p>{formatPopulation}</p>
     </div>
   );
 }
