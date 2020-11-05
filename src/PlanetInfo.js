7a4efa1365df20acc1cd039e690b3c40dbc56c94
Julia <yulyakonoval1998@gmail.com>
2020-11-05 11:35:16 +0200

    the residents fetched

--- src/PlanetInfo.js
+++ src/PlanetInfo.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
+import axios from "axios";
 
 function PlanetInfo() {
   const location = useLocation();
   const planet = location.state.planet;
+
+  const [planetResidents, setPlanetResidents] = useState([]);
+
+  useEffect(() => {
+    console.log(planet.residents);
+    let results = [];
+    async function fetchResidents() {
+      for (let resident of planet.residents) {
+        try {
+          const response = await axios.get(resident);
+          results = results.concat(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      setPlanetResidents(results);
+    }
+    fetchResidents();
+  }, []);
+
   return (
     <div className="planetInfo_container">
       <Link to="/">Back To Search</Link>
