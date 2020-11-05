f5d6f98b7b0de3595ade4e1cd7bd3fe832a3e3e4
Julia <yulyakonoval1998@gmail.com>
2020-11-05 12:12:59 +0200

    loader was created and styled

---
+++ src/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Spinner.css';
+
+function Spinner() {
+    return (
+        <div className="spinner">
+          <div className="lds-hourglass"> </div>  
+        </div>
+    )
+}
+
+Spinner.propTypes = {
+
+}
+
+export default Spinner
+
